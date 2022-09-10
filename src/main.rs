80261af1c12a4233706cf939db3aa6e1a8a028c2
Dmitriy Ivliev <rootkit89@gmail.com>
2022-09-10 12:54:23 +0300

    adds game over state

--- src/main.rs
+++ src/main.rs
@@ -60,6 +60,40 @@ impl State {
             monster_systems: build_monster_scheduler(),
         }
     }
+
+    fn game_over(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(2);
+        ctx.print_color_centered(2, RED, BLACK, "Your quest has ended.");
+        ctx.print_color_centered(
+            4,
+            WHITE,
+            BLACK,
+            "Slain by a monster, your hero's journey has come to a premature end.",
+        );
+        ctx.print_color_centered(
+            5,
+            WHITE,
+            BLACK,
+            "The Amulet of Yala remains unclaimed, and your home town is not saved.",
+        );
+        ctx.print_color_centered(8, GREEN, BLACK, "Press 1 to play again");
+        if let Some(VirtualKeyCode::Key1) = ctx.key {
+            self.ecs = World::default();
+            self.resources = Resources::default();
+            let mut rng = RandomNumberGenerator::new();
+            let builder = MapBuilder::new(&mut rng);
+            spawn_player(&mut self.ecs, builder.player_start);
+            builder
+                .rooms
+                .iter()
+                .skip(1)
+                .map(bracket_lib::prelude::Rect::center)
+                .for_each(|pos| spawn_enemy(&mut self.ecs, &mut rng, pos));
+            self.resources.insert(builder.map);
+            self.resources.insert(Camera::new(builder.player_start));
+            self.resources.insert(TurnState::AwaitingInput);
+        }
+    }
 }
 
 impl GameState for State {
@@ -83,6 +117,7 @@ impl GameState for State {
             TurnState::MonsterTurn => self
                 .monster_systems
                 .execute(&mut self.ecs, &mut self.resources),
+            TurnState::GameOver => self.game_over(ctx),
         }
         render_draw_buffer(ctx).expect("Render error");
     }
